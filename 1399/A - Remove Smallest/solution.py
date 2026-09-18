c07c2af2b4a8de0c3acd4b7cedd50d6a65d0d5f9
anujkhatri07 <anujkhatri844@gmail.com>
2026-09-18 21:54:14 +0530

    Add Remove Smallest [A] from Codeforces

---
+++ 1399/A - Remove Smallest/solution.py
@@ -0,0 +1,19 @@
+t = int(input())
+ 
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+ 
+    a.sort()
+ 
+    ok = True
+ 
+    for i in range(n - 1):
+        if a[i + 1] - a[i] > 1:
+            ok = False
+            break
+ 
+    if ok:
+        print("YES")
+    else:
+        print("NO")
