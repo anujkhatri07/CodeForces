9b2db3a2341d8b47a30eff1a26a128596d2c4d95
anujkhatri07 <anujkhatri844@gmail.com>
2026-09-24 22:26:32 +0530

    Add Halloumi Boxes [A] from Codeforces

---
+++ 1903/A - Halloumi Boxes/solution.py
@@ -0,0 +1,13 @@
+t = int(input())
+ 
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+ 
+    if k > 1:
+        print("YES")
+    else:
+        if a == sorted(a):
+            print("YES")
+        else:
+            print("NO")
